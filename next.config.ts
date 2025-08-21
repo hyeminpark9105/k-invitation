a3962818bf12c3ba8981baf7a78f32e3d0975f9e
hyeminpark9105 <hyeminpark9105@gmail.com>
2025-08-21 17:35:57 +0900

    :rocket: assetPrefix 제거

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   output: 'export',          // 정적 export
   images: { unoptimized: true }, // next/image 정적 처리
   basePath: isProd ? `/${repo}` : undefined,
-  assetPrefix: isProd ? `/${repo}/` : undefined,
+  // assetPrefix: isProd ? `/${repo}/` : undefined,
   trailingSlash: true,       // GitHub Pages 경로 이슈 줄이기
 }
 
